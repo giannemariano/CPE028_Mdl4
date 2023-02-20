b27676f3ef3cb43304b2dc844adfbcc0d8967bc6
giannemariano <qgcimariano@tip.edu.ph>
2023-02-20 10:47:32 +0000

    ems

---
+++ Class_Record.py
@@ -0,0 +1,14 @@
+import STUDENT
+import Grades
+
+p1 = STUDENT.STUDENT("Gianne Mariano", 20)
+p2 = Grades.Grades("CPE 028", 3, 1.25)
+
+print(p1)
+print(p2)
+print("Name:", p1.name)
+print("Age:", p1.age)
+print("Course Code:", p2.Course_Code)
+print("Course Units:",  p2.Course_Units)
+print("Course Grades:", p2.Course_Grade)
+    
